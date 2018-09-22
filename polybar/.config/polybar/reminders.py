9f3d73567dab30a1fd96af42928035b268d821a7
P1start <rewi-github@whanau.org>
2018-09-22 23:51:25 +1200

    Change reminders to refresh at 6am instead of midnight

--- polybar/.config/polybar/reminders.py
+++ polybar/.config/polybar/reminders.py
@@ -12,12 +12,13 @@ f = open('reminders').read()
 if f.strip() == '':
     sys.exit()
 
+current_date = datetime.datetime.now() - datetime.timedelta(hours=6)
 if len(sys.argv) > 1 and sys.argv[1] == 'dismiss':
     f = open('reminder-dismissed', 'w')
-    f.write(time.strftime('%Y-%m-%d'))
+    f.write(current_date.strftime('%Y-%m-%d'))
     f.close()
 
-if os.path.exists('reminder-dismissed') and open('reminder-dismissed').read().strip() == time.strftime('%Y-%m-%d'):
+if os.path.exists('reminder-dismissed') and open('reminder-dismissed').read().strip() == current_date.strftime('%Y-%m-%d'):
     sys.exit(1)
 
 highlight_colour = {i.split(':')[0]: i.split(':')[1].strip() for i in open(os.path.join(os.environ['HOME'], '.Xresources')).read().split('\n') if i.strip() != ''}
